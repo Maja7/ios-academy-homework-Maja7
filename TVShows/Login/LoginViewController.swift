e0f7d8bdb2a7120d2ec91d68bb43edb1a319d5c6
Maja Rukav <maja.rukav@gmail.com>
2019-07-05 14:54:03 +0200

    Deleted Main.Storyboard and ViewController.swift files and created new Group (Login). Created Login.storyboard and LoginViewController.swift  files.

---
+++ TVShows/Login/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  Login.swift
+//  TVShows
+//
+//  Created by Infinum on 05/07/2019.
+//  Copyright © 2019 Infinum Academy. All rights reserved.
+//
+
+import UIKit
+
+class Login: NSObject {
+
+}
