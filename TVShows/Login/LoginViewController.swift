1cd7cd50433e015d683796f631e6ee00e1ab678e
Maja Rukav <maja.rukav@gmail.com>
2019-07-08 11:29:17 +0200

    Created button and @IBACtion for button's Touch Up Inside event, created @IBOutlet for label (property/reference to the label UI element). Updated @IBACtion function so that on each tap, it will increment the number of taps, and show that in the label UI element. Extra: added color to the view controller and to the button, added rounded edges to the button, changed font and size on the label.

--- TVShows/Login/LoginViewController.swift
+++ TVShows/Login/LoginViewController.swift
@@ -1,13 +1,32 @@
 //
-//  Login.swift
+//  LoginViewController.swift
 //  TVShows
 //
-//  Created by Infinum on 05/07/2019.
+//  Created by Infinum on 08/07/2019.
 //  Copyright © 2019 Infinum Academy. All rights reserved.
 //
 
 import UIKit
 
-class Login: NSObject {
+class LoginViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        btnClick.layer.cornerRadius = 20
+        btnClick.clipsToBounds = true
+    }
+    var NumberOfTaps = 0
+    @IBAction func Mybutton(_ sender: Any) {
+        
+    
+        
+//    print("Any string you like!")
+        NumberOfTaps += 1
+        printOnLabel.text = String(NumberOfTaps)
+    }
+    
+    @IBOutlet weak var btnClick: UIButton!
+    @IBOutlet weak var printOnLabel: UILabel!
 }
