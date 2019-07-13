c244d6364aeea84875dec5beb675ba2d939b3ff5
Maja Rukav <maja.rukav@gmail.com>
2019-07-14 00:22:29 +0200

    Added new cocoapods into project.

--- TVShows/Pods/Target Support Files/Pods-TVShows/Pods-TVShows-frameworks.sh
+++ TVShows/Pods/Target Support Files/Pods-TVShows/Pods-TVShows-frameworks.sh
@@ -163,12 +163,16 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CodableAlamofire/CodableAlamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/LBTAComponents/LBTAComponents.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RevealingSplashView/RevealingSplashView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CodableAlamofire/CodableAlamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/LBTAComponents/LBTAComponents.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RevealingSplashView/RevealingSplashView.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
